c5cecd2bbc0f57601966d5d1261531ef446c7c59
beautiful-cookie <maxim.khadeev@gmail.com>
2023-01-06 00:28:20 +0300

    background and header styled

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,11 @@ const Header = (props) => {
     return (
         <header className={classes.headerWrapper}> 
             <span className={classes.logo}>Кино</span> 
-            <span></span> 
+            <div className={classes.headerElementsLogo}> 
+              <span className={classes.logoElement}>Главная</span>
+              <span className={classes.logoElement}>Поле 1</span>
+              <span className={classes.logoElement}>Поле 2</span>
+            </div> 
         </header> 
     )
 } 
