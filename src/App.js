8f7592d70e98c3ed34e877b713e9444464664fca
beautiful-cookie <maxim.khadeev@gmail.com>
2023-01-07 01:38:18 +0300

    film cards added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css'; 
 import Header from './components/Header/Header';
+import Movie from './components/Movie/Movie'; 
 
 
 function App() { 
   return (
     <div className='app-wrapper'>
       <Header /> 
+      <div className='movieWrapper'>
+        <Movie />  
+        <Movie />  
+        <Movie />  
+        <Movie />  
+        <Movie />  
+        <Movie />  
+        <Movie />  
+      </div> 
     </div> 
   );
 }
