208c12de45e04d1512ae4fdbe530cf9b8ed8a84b
beautiful-cookie <maxim.khadeev@gmail.com>
2023-01-30 23:41:49 +0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import './App.css'; 
+import React, { useState, useEffect } from 'react'; 
+import './App.css';  
 import Header from './components/Header/Header';
 import Movie from './components/Movie/Movie'; 
+import Search from './components/Search/Search';
 
 
 function App() { 
+
+  const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"; 
+
+  const [loader, setLoader] = useState(false); 
+  const [moviesArr, setMoviesArr] = useState([]); 
+  const [errMessage, setErrMessage] = useState(null); 
+
+  useEffect ( () => {
+    fetch(MOVIE_API_URL) 
+      .then(response => response.json()) 
+      .then(searchJSON => {
+        setMoviesArr(searchJSON.Search)  
+        console.log(searchJSON.Search)
+        setLoader(false) 
+      }) 
+  }, [])  
+
+  const search = searchValue => {
+    setLoader(true) 
+    setErrMessage(null) 
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`) 
+      .then(response => response.json())  
+      .then(responseJSON => {
+        if (responseJSON.Responce === 'True') {
+          setMoviesArr(responseJSON.Search)  
+          setLoader(false) 
+        } 
+        else {
+          setErrMessage(responseJSON.Error) 
+          setLoader(false) 
+        }
+      }) 
+  }
+  
   return (
     <div className='app-wrapper'>
       <Header /> 
-      <div className='movieWrapper'>
-        <Movie />  
-        <Movie />  
-        <Movie />  
-        <Movie />  
-        <Movie />  
-        <Movie />  
-        <Movie />  
+      <Search search={search} /> 
+      <div className='movieWrapper'> 
+        { loader && !errMessage ? (<span>loading...</span>) 
+        : errMessage 
+        ? (<div className='errMessage'>{errMessage}</div>) 
+        : moviesArr.map((movie, index) => (
+          <Movie key={`${index}-${movie.Title}`} movie={movie} />  
+        ))} 
       </div> 
     </div> 
   );
--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -4,18 +4,20 @@ import classes from './Movie.module.css';
 const DEFAULT_PLACEHOLDER_IMAGE  = 
 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpic.pikbest.com%2F01%2F77%2F96%2F38b888piCcZM.jpg-0.jpg!bw340&f=1&nofb=1&ipt=9c53cfddf65e5e134ec969563720728990f55f4f60279361f3dd8cabdd554343&ipo=images' 
 
-const Movie = ({movie}) => {
-  // const poster = 
-  //   movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster 
+const Movie = ({movie}) => { 
+
+  const poster = 
+    movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster 
+
     return (
       <div className={classes.posterWrapper}>
-        <span className={classes.filmTitle}>Film Text</span>
+        <span className={classes.filmTitle}>{movie.Title}</span>
         <div className={classes.imgWrapper}>
           <img 
-          src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpic.pikbest.com%2F01%2F77%2F96%2F38b888piCcZM.jpg-0.jpg!bw340&f=1&nofb=1&ipt=9c53cfddf65e5e134ec969563720728990f55f4f60279361f3dd8cabdd554343&ipo=images' 
-          alt="" />
+          src={poster} 
+          alt="" /> 
         </div> 
-        <span className={classes.date}>(2000)</span>
+        <span className={classes.date}>({ movie.Year })</span> 
       </div>
     ); 
 } 
