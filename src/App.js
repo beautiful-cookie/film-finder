d735b9c3f19a7ccd7434dbab95edd1d68b3ba072
beautiful-cookie <maxim.khadeev@gmail.com>
2023-02-05 21:26:20 +0300

    logic bug fix

--- src/App.js
+++ src/App.js
@@ -1,46 +1,87 @@
-import React, { useState, useEffect } from 'react'; 
+import React, { useState, useEffect, useReducer } from 'react'; 
 import './App.css';  
 import Header from './components/Header/Header';
 import Movie from './components/Movie/Movie'; 
 import Search from './components/Search/Search';
 
 
+const initialState = {
+  loading: true, 
+  errorMessage: null, 
+  moviesArr: [] 
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SEARCH_MOVIES_REQUEST':
+      return {
+        ...state, 
+        loader: true, 
+        errMessage: null 
+      }      
+      case 'SEARCH_MOVIES_SUCCESS':
+        return { 
+          ...state, 
+          loader: false, 
+          moviesArr: action.insideJSON 
+        }   
+        case 'SEARCH_MOVIES_FAILURE':
+          return {
+            ...state, 
+            loader: false, 
+            errMessage: action.error  
+          }   
+  
+    default:
+      return state;
+  }
+}
+
 function App() { 
+  const [state, dispatch] = useReducer(reducer, initialState) 
 
   const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"; 
 
-  const [loader, setLoader] = useState(false); 
-  const [moviesArr, setMoviesArr] = useState([]); 
-  const [errMessage, setErrMessage] = useState(null); 
 
   useEffect ( () => {
     fetch(MOVIE_API_URL) 
       .then(response => response.json()) 
-      .then(searchJSON => {
-        setMoviesArr(searchJSON.Search)  
-        console.log(searchJSON.Search)
-        setLoader(false) 
+      .then(searchJSON => { 
+
+        dispatch({
+          type: 'SEARCH_MOVIES_SUCCESS', 
+          insideJSON: searchJSON.Search, 
+        }) 
+        
       }) 
   }, [])  
 
-  const search = searchValue => {
-    setLoader(true) 
-    setErrMessage(null) 
+  const search = searchValue => { 
+
+    dispatch({
+      type: 'SEARCH_MOVIES_REQUEST', 
+    })
 
     fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`) 
       .then(response => response.json())  
       .then(responseJSON => {
-        if (responseJSON.Responce === 'True') {
-          setMoviesArr(responseJSON.Search)  
-          setLoader(false) 
+        if (responseJSON.Response === 'True') { 
+          dispatch({ 
+            type: 'SEARCH_MOVIES_SUCCESS', 
+            insideJSON: responseJSON.Search, 
+          }) 
         } 
-        else {
-          setErrMessage(responseJSON.Error) 
-          setLoader(false) 
+        else { 
+          dispatch({
+            type: 'SEARCH_MOVIES_FAILURE', 
+            error: responseJSON.Error 
+          }) 
         }
       }) 
   }
   
+const {errMessage, loader, moviesArr} = state; 
+
   return (
     <div className='app-wrapper'>
       <Header /> 
